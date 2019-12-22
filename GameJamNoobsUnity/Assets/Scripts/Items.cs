271b651d4ce03a66b18013c3a29e2640283c0560
Brian Leip <brianleip@gmail.com>
2019-12-21 21:15:24 -0800

    Rudolph is movin

--- GameJamNoobsUnity/Assets/Scripts/Items.cs
+++ GameJamNoobsUnity/Assets/Scripts/Items.cs
@@ -6,22 +6,31 @@ using UnityEngine.UI;
 public class Items : MonoBehaviour
 {
     public float speed = 0.07f;      // default to 0.07
-    private Transform item;
+    
+    private GameObject fxPrefab;
+    private Transform itemTfm;
     // Start is called before the first frame update
     void Start()
     {
-        item = GetComponent<Transform>();
+        itemTfm = GetComponent<Transform>();
+        fxPrefab = GameObject.Find("StarBurst2D");
     }
 
     // Update is called once per frame
     void Update()
     {
         Vector3 newLoc = new Vector3(-speed,0,0);
-        item.Translate(newLoc);
+        itemTfm.Translate(newLoc);
     }
 
-    void OnTriggerEnter2D() {
-        Destroy(this.gameObject);
+    void OnTriggerEnter2D(Collider2D col) {
+        if (col.gameObject.tag == "Player") {
+            fxPrefab.SetActive(true);
+            GameObject fxCollect = Instantiate(fxPrefab, itemTfm.position, Quaternion.identity);
+            // fxCollect.SetActive(true);
+            Destroy(this.gameObject);
+        }
+
     }
 
 
--- GameJamNoobsUnity/Assets/Scripts/PlayerController.cs
+++ GameJamNoobsUnity/Assets/Scripts/PlayerController.cs
@@ -4,18 +4,50 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float accel = 2f;
+    public float drag = 0.2f;    
 
-    Rigidbody2D rb2d;
+    private Rigidbody2D rb2d;
+    private bool movingUp, movingDown, movingLeft, movingRight = false;
 
     // Start is called before the first frame update
     void Start()
     {
-    Rigidbody2D rb2d = GetComponent<Rigidbody2D>();
+        rb2d = GetComponent<Rigidbody2D>();
+        rb2d.velocity = Vector2.zero;
+        rb2d.drag = drag;
     }
 
     // Update is called once per frame
-    void Update()
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.W) ) {  movingUp = true;  }
+        if (Input.GetKeyDown(KeyCode.S) ) {  movingDown = true;  }
+        if (Input.GetKeyDown(KeyCode.A) ) {  movingLeft = true;  }
+        if (Input.GetKeyDown(KeyCode.D) ) {  movingRight = true;  }
+
+        if (Input.GetKeyUp(KeyCode.W) ) {  movingUp = false;  }
+        if (Input.GetKeyUp(KeyCode.S) ) {  movingDown = false;  }
+        if (Input.GetKeyUp(KeyCode.A) ) {  movingLeft = false;  }
+        if (Input.GetKeyUp(KeyCode.D) ) {  movingRight = false;  }
+    }
+
+    void FixedUpdate()
     {
-        
+        if (movingUp) {
+            Debug.Log("Up");
+            rb2d.velocity += (Vector2.up * accel);
+        }
+        if (movingDown) {
+            Debug.Log("Down");
+            rb2d.velocity += (Vector2.down * accel);
+        }
+        else if (movingLeft) {
+            Debug.Log("Left");
+            rb2d.velocity += (Vector2.left * accel);
+        }
+        else if (movingRight) {
+            Debug.Log("Right");
+            rb2d.velocity += (Vector2.right * accel);
+        }
     }
 }
