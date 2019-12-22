057a7d716f05a2cde2fb5ccbb4039384e5a6e729
Brian Leip <brianleip@gmail.com>
2019-12-21 18:38:59 -0800

    Created item script and made a few prefabs

--- GameJamNoobsUnity/Assets/Scripts/Items.cs
+++ GameJamNoobsUnity/Assets/Scripts/Items.cs
@@ -3,20 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class TerrainMover : MonoBehaviour
+public class Items : MonoBehaviour
 {
     public float speed = 0.07f;      // default to 0.07
-    private RectTransform terrain;
+    private Transform item;
     // Start is called before the first frame update
     void Start()
     {
-        terrain = GetComponent<RectTransform>();
+        item = GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
         Vector3 newLoc = new Vector3(-speed,0,0);
-        terrain.Translate(newLoc);
+        item.Translate(newLoc);
     }
+
+    void OnTriggerEnter2D() {
+        Destroy(this.gameObject);
+    }
+
+
 }
---
+++ GameJamNoobsUnity/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    Rigidbody2D rb2d;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    Rigidbody2D rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
