8cebf02759b95d578bcb82ed6e5abffec528c607
Brian Leip <brianleip@gmail.com>
2019-12-21 17:37:06 -0800

    Made terrain mover script and added to Main scene

---
+++ GameJamNoobsUnity/Assets/Scripts/TerrainMover.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TerrainMover : MonoBehaviour
+{
+    public float speed = 0;
+    private RectTransform terrain;
+    // Start is called before the first frame update
+    void Start()
+    {
+        terrain = GetComponent<RectTransform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newLoc = new Vector3(-speed,0,0);
+        terrain.Translate(newLoc);
+    }
+}
