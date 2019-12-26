93f6494d9013c15289651d6c8908415fe135c1a7
Brian Leip <brianleip@gmail.com>
2019-12-25 17:22:53 -0800

    got the new Input system to log messages when buttons pushed on gamepad, keyboard, or mouse

--- GameJamNoobsUnity/Assets/Scripts/PlayerController.cs
+++ GameJamNoobsUnity/Assets/Scripts/PlayerController.cs
@@ -1,9 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;  // new unity namespace for consolidated input handling
 
 public class PlayerController : MonoBehaviour
 {
+    // MyPlayerControls is the C# class that Unity generated.
+    // It encapsulates the data from the .inputactions asset we created
+    // and automatically looks up all the maps and actions for us.
+    // MyPlayerControls controls;
+
     public float accel = 2f;
     public float drag = 0.2f;    
 
@@ -11,11 +17,21 @@ public class PlayerController : MonoBehaviour
     private bool movingUp, movingDown, movingLeft, movingRight = false;
 
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         rb2d = GetComponent<Rigidbody2D>();
         rb2d.velocity = Vector2.zero;
         rb2d.drag = drag;
+    }
+
+    public void OnUse(InputAction.CallbackContext context) {
+        // 'Use' code here.
+        Debug.Log(context);
+    }
+
+    public void OnMove(InputAction.CallbackContext context) {
+        // 'Move' code here.
+        Debug.Log(context);
+        
     }
 
     // Update is called once per frame
@@ -33,6 +49,18 @@ public class PlayerController : MonoBehaviour
 
     void FixedUpdate()
     {
+        var gamepad = Gamepad.current;
+
+        if (gamepad == null)
+            return;     // no gamepad connected
+
+        if (gamepad.rightTrigger.wasPressedThisFrame) {
+            // 'Use' code here
+        }
+
+        Vector2 move = gamepad.leftStick.ReadValue();
+        // 'Move' code here
+
         if (movingUp) {
             Debug.Log("Up");
             rb2d.velocity += (Vector2.up * accel);
